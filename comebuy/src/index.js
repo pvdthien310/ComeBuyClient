c2f237aacbf849bc45cfe36efd03e9ce6adee9d2
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2022-03-26 15:35:53 +0700

    ok

--- comebuy/src/index.js
+++ comebuy/src/index.js
@@ -8,11 +8,15 @@ import { Provider } from 'react-redux'
 import store from './redux/store.js'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <Provider store={store}>
+  //     <App />
+  //   </Provider>
+  // </React.StrictMode>,
+
+   <Provider store={store}>
+     <App />
+   </Provider>,
   document.getElementById('root')
 );
 
