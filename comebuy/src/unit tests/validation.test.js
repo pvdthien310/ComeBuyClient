ccc4dedf8868d4a7e5909f28c18744433d2c0246
HuynhVong <71302943+HuynhVong@users.noreply.github.com>
2022-04-24 16:13:00 +0700

    ok

--- comebuy/src/unit tests/validation.test.js
+++ comebuy/src/unit tests/validation.test.js
@@ -1,5 +1,36 @@
 import * as validation from '../container/LoginAndRegister/ValidationDataForAccount'
 
+// //test email
+// test("Check Email", () => {
+//   const result = validation.CheckEmail("theostevenson1880@gmail.com")
+//   expect(result).toBe(true);
+// })
+
+// test("Check Email", () => {
+//   const result = validation.CheckEmail("theostevenson1880@@gmail.com")
+//   expect(result).toBe(false);
+// })
+
+// test("Check Email", () => {
+//   const result = validation.CheckEmail("theostevenson1880gmail.com")
+//   expect(result).toBe(false);
+// })
+
+// test("Check Email", () => {
+//   const result = validation.CheckEmail(" ")
+//   expect(result).toBe(false);
+// })
+
+// test("Check Email", () => {
+//   const result = validation.CheckEmail("theostevenson1880@gmailcom")
+//   expect(result).toBe(false);
+// })
+
+// test("Check Email", () => {
+//   const result = validation.CheckEmail("theostevenson1880gmailcom")
+//   expect(result).toBe(false);
+// })
+
 //test CheckPassword
 test("Check password", () => {
   const result = validation.CheckPassword("Vongminhhuynh1880")
@@ -21,60 +52,59 @@ test("Check password", () => {
   expect(result).toBe(false);
 })
 
-//test email
-test("Check Email", () => {
-  const result = validation.CheckPassword("theostevenson1880@gmail.com")
-  expect(result).toBe(true);
-})
+// //Check Username
+// test("Check Username", () => {
+//   const result = validation.CheckUsername("Customer1")
+//   expect(result).toBe(false);
+// })
 
-test("Check Email", () => {
-  const result = validation.CheckPassword("theostevenson1880gmail.com")
-  expect(result).toBe(false);
-})
+// test("Check Username", () => {
+//   const result = validation.CheckUsername("Customer")
+//   expect(result).toBe(false);
+// })
 
-test("Check Email", () => {
-  const result = validation.CheckPassword("theostevenson1880@gmailcom")
-  expect(result).toBe(false);
-})
+// test("Check Username", () => {
+//   const result = validation.CheckUsername(" ")
+//   expect(result).toBe(true);
+// })
 
-test("Check Email", () => {
-  const result = validation.CheckPassword("theostevenson1880gmailcom")
-  expect(result).toBe(false);
-})
-
-//Check Username
-test("Check Username", () => {
-  const result = validation.CheckPassword("Customer1")
-  expect(result).toBe(true);
-})
-
-test("Check Username", () => {
-  const result = validation.CheckPassword("Customer")
-  expect(result).toBe(false);
-})
-
-test("Check Username", () => {
-  const result = validation.CheckPassword("@.")
-  expect(result).toBe(false);
-})
+// test("Check Username", () => {
+//   const result = validation.CheckUsername("@.")
+//   expect(result).toBe(true);
+// })
 
 //Check phone number
-test("Check Phone Number", () => {
-  const result = validation.CheckPassword("0235985741")
-  expect(result).toBe(true);
-})
+// test("Check Phone Number", () => {
+//   const result = validation.CheckPhoneNumber("0235985741")
+//   expect(result).toBe(true);
+// })
 
-test("Check Phone Number", () => {
-  const result = validation.CheckPassword("02359857411")
-  expect(result).toBe(true);
-})
+// test("Check Phone Number", () => {
+//   const result = validation.CheckPhoneNumber("02359857411")
+//   expect(result).toBe(true);
+// })
 
-test("Check Phone Number", () => {
-  const result = validation.CheckPassword("1021598652")
-  expect(result).toBe(false);
-})
+// test("Check Phone Number", () => {
+//   const result = validation.CheckPhoneNumber("0235aa5741")
+//   expect(result).toBe(false);
+// })
 
-test("Check Phone Number", () => {
-  const result = validation.CheckPassword("0245")
-  expect(result).toBe(false);
-})
+// test("Check Phone Number", () => {
+//   const result = validation.CheckPhoneNumber("1021598652")
+//   expect(result).toBe(false);
+// })
+
+// test("Check Phone Number", () => {
+//   const result = validation.CheckPhoneNumber(" ")
+//   expect(result).toBe(false);
+// })
+
+// test("Check Phone Number", () => {
+//   const result = validation.CheckPhoneNumber("aa")
+//   expect(result).toBe(false);
+// })
+
+// test("Check Phone Number", () => {
+//   const result = validation.CheckPhoneNumber("0245")
+//   expect(result).toBe(false);
+// })
