cec9e832529a2c8dd9464fee968d00845dfb810e
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2022-09-15 11:01:07 +0700

    fix eslint

--- comebuy/src/GridDataCellTemplate/ImportantTag.js
+++ comebuy/src/GridDataCellTemplate/ImportantTag.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import clsx from 'clsx';
 import { alpha, styled } from '@mui/material/styles';
 import { jsx as _jsx } from 'react/jsx-runtime';
+
 const Value = styled('div')(({ theme }) => ({
     width: '100%',
     height: '100%',
@@ -22,18 +23,19 @@ const currencyFormatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
 });
-const TotalPrice = /*#__PURE__*/ React.memo(function TotalPrice(props) {
+const TotalPrice = /* #__PURE__ */ React.memo((props) => {
     const { value, delimitedValue } = props;
-    return /*#__PURE__*/ _jsx(Value, {
+    return /* #__PURE__ */ _jsx(Value, {
         className: clsx(value > delimitedValue && 'good', value <= delimitedValue && 'bad'),
         children: currencyFormatter.format(value),
     });
 });
 
-//// delimitedValue la gia tri phan dinh giua good and bad
+// delimitedValue la gia tri phan dinh giua good and bad
 
+// eslint-disable-next-line import/prefer-default-export
 export function renderImportantTag(params, _delimitdeValue) {
-    return /*#__PURE__*/ _jsx(TotalPrice, {
+    return /* #__PURE__ */ _jsx(TotalPrice, {
         value: params,
         delimitedValue: _delimitdeValue,
     });
